602cafc408c95fe22b19c8c11b81584cad1af593
LucasMagic <lucas.cunin@magiclearning.fr>
2023-12-19 01:59:22 +0100

    1_er_script

---
+++ llama_test_1.py
@@ -0,0 +1,19 @@
+import os 
+from llama_cpp import Llama
+
+model_path = os.getenv("MODEL_PATH")
+
+# Create a Llama instance with a specified model
+llm = Llama(model_path= model_path)
+
+# Define the prompt and the generation parameters
+prompt = "Quelle est la puissance de llama2 ?"
+max_tokens = 400  # Generate up to 400 tokens
+
+# Generate a completion based on the prompt and parameters
+output = llm(prompt,
+             max_tokens=max_tokens,
+             echo=True)
+
+# Print the generated output
+print(output)
